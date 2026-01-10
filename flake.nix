2cf9cf7c208d80f5389adb528845ab50e663af71
Doug Campos <qmx@qmx.me>
2026-01-09 23:53:56 -0300

    Add Nix flake for reproducible dev environment
    Sets up Rust toolchain with rust-src and rust-analyzer extensions,
    plus pkg-config and openssl dependencies.

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Sterna - Git-native issue tracker";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+          ];
+        };
+      }
+    );
+}
